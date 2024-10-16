7e5532634fe62f6cf92a3c95d2ad0b1b14f365e8
hannahf86 <hannahfeehan.design@gmail.com>
2024-10-16 12:26:37 +0100

    image gallery fixed

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -5,28 +5,28 @@ import "react-image-gallery/styles/css/image-gallery.css";
 const Slider = () => {
   const images = [
     {
-      original: "../Assets/Images/Slideshow/ss-01.webp",
-      thumbnail: "../Assets/Images/Thumbnails/tn-01.webp",
+      original: "/ss-01.webp",
+      thumbnail: "/tn-01.webp",
     },
     {
-      original: "../Assets/Images/Slideshow/ss-02.webp",
-      thumbnail: "../Assets/Images/Thumbnails/tn-02.webp",
+      original: "/ss-02.webp",
+      thumbnail: "/tn-02.webp",
     },
     {
-      original: "../Assets/Images/Slideshow/ss-03.webp",
-      thumbnail: "../Assets/Images/Thumbnails/tn-03.webp",
+      original: "/ss-03.webp",
+      thumbnail: "/tn-03.webp",
     },
     {
-      original: "../Assets/Images/Slideshow/ss-04.webp",
-      thumbnail: "../Assets/Images/Thumbnails/tn-04.webp",
+      original: "/ss-04.webp",
+      thumbnail: "/tn-04.webp",
     },
     {
-      original: "../Assets/Images/Slideshow/ss-05.webp",
-      thumbnail: "../Assets/Images/Thumbnails/tn-05.webp",
+      original: "/ss-05.webp",
+      thumbnail: "/tn-05.webp",
     },
     {
-      original: "../Assets/Images/Slideshow/ss-06.webp",
-      thumbnail: "../Assets/Images/Thumbnails/tn-06.webp",
+      original: "/ss-06.webp",
+      thumbnail: "/tn-06.webp",
     },
   ];
 
